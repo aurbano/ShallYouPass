94cbe1be28e72f211f47f8f24f9c412ff7d946a2
Alejandro U. Alvarez <aluralma@gmail.com>
2015-02-16 10:56:31 +0000

    Initial commit

---
+++ src/ShallYouPass.js
@@ -0,0 +1,14 @@
+/**
+ * ShallYouPass
+ * Nodejs password analysis tool to prevent identity theft
+ *
+ * @author Alejandro U. Alvarez (urbanoalvarez.es)
+ * @license AGPLv3
+ */
+
+var ShallYouPass = function(opts) {
+  //TODO Use $.extend for the options
+  var sp = opts;
+
+
+};
